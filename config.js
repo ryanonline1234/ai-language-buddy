e3105c42cd5e185b3525ce34ba5efea45ed9e79b
ryanonline1234 <ryanonline2011c@gmail.com>
2025-08-21 21:19:44 -0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBBdkxJaZbunNM9aVET6y7QFNtK5LcA9ao",
+    authDomain: "test-b30c1.firebaseapp.com",
+    projectId: "test-b30c1",
+    storageBucket: "test-b30c1.appspot.com",
+    messagingSenderId: "35670538003",
+    appId: "1:35670538003:web:9725ede5f8d5aaf0e58ce6"
+  };
+
+const GEMINI_API_KEY = 'AIzaSyD2FnyoMae5_8qkDnWmzsrt6GrBpUx9GKk';
